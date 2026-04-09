dfc7bff6dcf0dee8d929c009af174bec9578c04d
YandoNomenY <142489981+YandoNomenY@users.noreply.github.com>
2026-04-09 16:01:13 +0300

    Clean up ThemeToggle component
    Remove commented-out code for theme icon rendering.

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -35,11 +35,11 @@ export const ThemeToggle = () => {
             "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", 
             "focus:outlin-hidden"
         )}> 
-            {/* {isDarkMode ? (
+             {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300"/>
             ) : (
                 <Moon className="h-6 w-6 text-blue-900"/>
-                )}  */}
+                )}  
         </button>
     )
 }
