334e4896a3f5e978abd4e2407bc9e1b164c8666b
YandoNomenY <142489981+YandoNomenY@users.noreply.github.com>
2026-04-09 14:47:37 +0300

    Correct theme toggle functionality
    Fix theme toggle logic to correctly set light and dark modes.

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -18,8 +18,8 @@ export const ThemeToggle = () => {
     const toggleTheme = () => {
        if (isDarkMode) {
             document.documentElement.classList.remove("dark");
-            localStorage.setItem("theme", "light");
-            setIsDarkMode(false)
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true)
        } else {
             document.documentElement.classList.add("dark");
             localStorage.setItem("theme", "dark");
