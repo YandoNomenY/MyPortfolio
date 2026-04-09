a9e9d1014905da3d6e0313f13010727bc141d818
YandoNomenY <142489981+YandoNomenY@users.noreply.github.com>
2026-04-09 15:18:50 +0300

    Change default theme mode to light

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react"
 import {Moon, Sun} from "lucide-react"
 import { cn } from "@/lib/utils";
 
+
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
@@ -19,11 +20,11 @@ export const ThemeToggle = () => {
        if (isDarkMode) {
             document.documentElement.classList.remove("dark");
             localStorage.setItem("theme", "dark");
-            setIsDarkMode(true)
+            setIsDarkMode(false)
        } else {
             document.documentElement.classList.add("dark");
             localStorage.setItem("theme", "dark");
-            setIsDarkMode(true)
+            setIsDarkMode(false)
        }
     }
 
@@ -34,11 +35,11 @@ export const ThemeToggle = () => {
             "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", 
             "focus:outlin-hidden"
         )}> 
-            {isDarkMode ? (
+            {/* {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300"/>
             ) : (
                 <Moon className="h-6 w-6 text-blue-900"/>
-                )} 
+                )}  */}
         </button>
     )
 }
